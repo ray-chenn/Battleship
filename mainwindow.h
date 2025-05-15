67b7ff42784dd273422fb10a99cade170cee1489
Ray Chen <63975805+ray-chenn@users.noreply.github.com>
2025-05-15 16:04:53 -0400

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -3,11 +3,15 @@
 
 #include <QMainWindow>
 
+class Board;
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
+
+
 {
     Q_OBJECT
 
@@ -17,5 +21,8 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    bool shipGrid[10][10] = {};
+    bool shotGrid[10][10] = {};
 };
+
 #endif // MAINWINDOW_H
